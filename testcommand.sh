351aef9a84acb3b3475eee9863e633dea91fbeaa
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-10-29 20:57:07 +0300

    test commands

--- testcommand.sh
+++ testcommand.sh
@@ -2,18 +2,18 @@
 echo "Updating Packages ..."
 sudo su
 pacman -Suy --noconfirm
-echo "Installing Necessary Packages ..."
-pacman -S --noconfirm xorg xorg-xinit arandr bspwm sxhkd dmenu feh picom firefox rxvt-unicode
+#echo "Installing Necessary Packages ..."
+#pacman -S --noconfirm xorg xorg-xinit arandr bspwm sxhkd dmenu feh picom firefox rxvt-unicode
 exit
 
 # Setting Up BSPWM
-echo "Setting up BSPWM ..."
-mkdir ~/.config
-mkdir ~/.config/bspwm
-mkdir ~/.config/sxhkd
-cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
-cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
+#echo "Setting up BSPWM ..."
+#mkdir ~/.config
+#mkdir ~/.config/bspwm
+#mkdir ~/.config/sxhkd
+#cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
+#cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
 
 # Setting up .xinit
-echo "Setting up .xinitrc ..."
-cp /etc/X11/xinit/xinitrc ~/.xinitrc
+#echo "Setting up .xinitrc ..."
+#cp /etc/X11/xinit/xinitrc ~/.xinitrc
