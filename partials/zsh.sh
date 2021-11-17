12595b5542b7d19861e7cd5d7a47421844980555
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-18 00:09:25 +0300

    Minor Changes

--- partials/zsh.sh
+++ partials/zsh.sh
@@ -5,5 +5,5 @@ source "$PWD/utils.sh"
 cecho "RED" "Installing ZSH  ..."
 sudo pacman --noconfirm --needed -S zsh
 cecho "RED" "Setting ZSH as default ..."
-chsh -s `which zsh`
+chsh -s "$(which zsh)"
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
