8124ec1cdeb3d2f9a7748e53bf1758a9d730d726
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-18 02:11:12 +0300

    Some Experiments

--- basic_install.sh
+++ basic_install.sh
@@ -3,8 +3,15 @@
 git clone https://github.com/aminzabardast/dotfiles.git "$HOME/.dotfiles"
 cd "$HOME/.dotfiles"
 
+source "$PWD/utils.sh"
+
 # Install ZSH
-sh "$PWD/partials/zsh.sh"
+cecho "RED" "Installing ZSH  ..."
+sudo pacman --noconfirm --needed -S zsh
+cecho "RED" "Setting ZSH as default ..."
+chsh -s "$(which zsh)"
+curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
+
 # Update
 sh "$PWD/partials/update_and_upgrade.sh"
 # Install Tmux
