92e8c3d9e7a9dd7abf720bb29be33f5a0ae0eb2b
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-17 14:51:34 +0300

    Test Code Partialized

--- testcommand.sh
+++ testcommand.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 # Update
+sh partials/update_or_upgrade.sh
 # Install ZSH
+sh partials/zsh.sh
 # Install Tmux
+sh partials/tmux.sh
 # Install fzf
+sh partials/fzf.sh
 # gnome or bspwm
+sh partials/gnome.sh
