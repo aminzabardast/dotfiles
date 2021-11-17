d1b6b0c7a6faae61abd2e6dd5077a983f07b859c
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-18 01:57:23 +0300

    Some Experiments

--- basic_install.sh
+++ basic_install.sh
@@ -3,13 +3,13 @@
 git clone https://github.com/aminzabardast/dotfiles.git "$HOME/.dotfiles"
 cd "$HOME/.dotfiles"
 
-# Update
-sh "$PWD/partials/update_and_upgrade.sh"
 # Install ZSH
 sh "$PWD/partials/zsh.sh"
-# # Install Tmux
-# sh "$PWD/partials/tmux.sh"
-# # Install fzf
-# sh "$PWD/partials/fzf.sh"
-# # gnome or bspwm
-# sh "$PWD/partials/gnome.sh"
+# Update
+sh "$PWD/partials/update_and_upgrade.sh"
+# Install Tmux
+sh "$PWD/partials/tmux.sh"
+# Install fzf
+sh "$PWD/partials/fzf.sh"
+# gnome or bspwm
+sh "$PWD/partials/gnome.sh"
