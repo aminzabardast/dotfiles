93b5b8c2b58ccb9314404e9702467d2cc866906e
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-18 21:05:43 +0300

    Installing NVM

--- partials/nvm.sh
+++ partials/nvm.sh
@@ -8,3 +8,7 @@ cecho "RED" "Installing Node.js ..."
 nvm install --lts
 cecho "RED" "Updating NPM ..."
 npm install -g npm
+echo "
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+" >> .profile
