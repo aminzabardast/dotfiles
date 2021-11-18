d5a5fb45dc58d8abd5b743fd7472602d42af19f7
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-18 21:13:23 +0300

    Installing NVM

--- partials/nvm.sh
+++ partials/nvm.sh
@@ -5,9 +5,9 @@ source "$PWD/utils.sh"
 cecho "RED" "Installing NVM ..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | sh
 echo "
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-" >> .profile
+export NVM_DIR=\"$([ -z \"${XDG_CONFIG_HOME-}\" ] && printf %s \"${HOME}/.nvm\" || printf %s \"${XDG_CONFIG_HOME}/nvm\")\"
+[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\" # This loads nvm
+" > "$HOME/.profile"
 source "$HOME/.profile"
 cecho "RED" "Installing Node.js ..."
 nvm install --lts
