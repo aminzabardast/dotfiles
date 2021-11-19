dbf3e3f6ceaa981a0bd63b7eab3ded05e7b53008
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-19 18:23:31 +0300

    minor fix

--- partials/albert.sh
+++ partials/albert.sh
@@ -7,6 +7,6 @@ sudo git clone https://aur.archlinux.org/albert.git /opt/albert
 sudo chown -R $USER:$USER /opt/albert
 cd /opt/albert
 makepkg --noconfirm --needed -si
-cp "config/albert" "$HOME/.config/albert"
+cp "$PWD/config/albert" "$HOME/.config/albert"
 mkdir -p "$HOME/.config/autostart"
-cp "config/autostart/albert.desktop" "$HOME/.config/autostart/albert.desktop"
+cp "$PWD/config/autostart/albert.desktop" "$HOME/.config/autostart/albert.desktop"
