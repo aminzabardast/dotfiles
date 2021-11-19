4ee73bbb58fdfcd68590dc195d6c2a7e690f144f
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-19 20:52:41 +0300

    Gnome Extentions

--- partials/gnome.sh
+++ partials/gnome.sh
@@ -3,7 +3,12 @@
 source "$PWD/utils.sh"
 
 cecho "RED" "Setting up Gnome ..."
-sudo pacman --noconfirm --needed -S gnome gnome-extra
+sudo pacman --noconfirm --needed -S gnome gnome-extra gnome-shell-extension-appindicator libappindicator-gtk2 libappindicator-gtk3
 
 cecho "RED" "Enabling GDM ..."
 sudo systemctl enable gdm
+
+cecho "RED" "Enabling Extentions ..."
+gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
+gnome-extensions enable drive-menu@gnome-shell-extensions.gcampax.github.com
+gnome-extensions enable places-menu@gnome-shell-extensions.gcampax.github.com
