8de3772ac98d6b051498a2d446001e4bf6ed5197
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-19 22:44:46 +0300

    Minor Fix

---
+++ partials/fdm.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+source "$PWD/utils.sh"
+
+cecho "RED" "Installing Free Download Manager ..."
+sudo git clone https://aur.archlinux.org/freedownloadmanager.git /opt/fdm
+sudo chown -R $USER:$USER /opt/fdm
+cd "/opt/fdm"
+makepkg --noconfirm --needed -si
+cp "$PWD/config/autostart/FDM.desktop" "$HOME/.config/autostart/FDM.desktop"
