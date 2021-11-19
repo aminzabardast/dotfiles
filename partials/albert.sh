a649bcf12101d5689302eee524a6be6de70cf719
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-19 16:43:47 +0300

    Minor Fix

--- partials/albert.sh
+++ partials/albert.sh
@@ -3,7 +3,7 @@
 source "$PWD/utils.sh"
 
 cecho "RED" "Installing Albert ..."
-git clone https://aur.archlinux.org/albert.git /opt/albert
+sudo git clone https://aur.archlinux.org/albert.git /opt/albert
 sudo chown -R $USER:$USER /opt/albert
 cd /opt/albert
 makepkg -si
