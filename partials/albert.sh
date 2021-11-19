87354bd41eda79f280f0008ffefdbd5876e9a9e1
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-19 18:46:00 +0300

    minor fix

--- partials/albert.sh
+++ partials/albert.sh
@@ -5,8 +5,9 @@ source "$PWD/utils.sh"
 cecho "RED" "Installing Albert ..."
 sudo git clone https://aur.archlinux.org/albert.git /opt/albert
 sudo chown -R $USER:$USER /opt/albert
-cd /opt/albert
+cd "/opt/albert"
 makepkg --noconfirm --needed -si
+cd "$HOME/.dotfiles"
 cp "$PWD/config/albert" "$HOME/.config/albert"
 mkdir -p "$HOME/.config/autostart"
 cp "$PWD/config/autostart/albert.desktop" "$HOME/.config/autostart/albert.desktop"
