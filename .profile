fd2ec72e03f7c450419150f2821d0103a3ae1f67
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-21 14:33:02 +0300

    Using Symlinks - Some Minor Fixes #6

--- .profile
+++ .profile
@@ -1,3 +1,9 @@
 # NVM stuff
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+#Pyenv Stuff
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
--- partials/zsh.sh
+++ partials/zsh.sh
@@ -7,6 +7,6 @@ sudo pacman --noconfirm --needed -S zsh
 cecho "RED" "Setting ZSH as default ..."
 chsh -s "$(which zsh)"
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
-cp "$PWD/.zshrc" "$HOME/.zshrc"
-cp "$PWD/.profile" "$HOME/.profile"
-cp "$PWD/.personal" "$HOME/.personal"
+ln -s "$PWD/.zshrc" "$HOME/.zshrc"
+ln -s "$PWD/.profile" "$HOME/.profile"
+ln -s "$PWD/.personal" "$HOME/.personal"
