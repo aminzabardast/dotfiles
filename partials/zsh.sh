67ab6930c53df27167a420fa9a84fd6dbb610922
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-21 16:18:31 +0300

    Minor Fix

--- partials/zsh.sh
+++ partials/zsh.sh
@@ -7,6 +7,6 @@ sudo pacman --noconfirm --needed -S zsh
 cecho "RED" "Setting ZSH as default ..."
 chsh -s "$(which zsh)"
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
-ln -s "$PWD/.zshrc" "$HOME/.zshrc"
-ln -s "$PWD/.profile" "$HOME/.profile"
-ln -s "$PWD/.personal" "$HOME/.personal"
+ln -sf "$PWD/.zshrc" "$HOME/.zshrc"
+ln -sf "$PWD/.profile" "$HOME/.profile"
+ln -sf "$PWD/.personal" "$HOME/.personal"
