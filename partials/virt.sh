eae97ee3fcd07a4df17af1f3e2480700381e3e6f
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-12-03 13:16:10 +0300

    Minor Fix

--- partials/virt.sh
+++ partials/virt.sh
@@ -3,4 +3,4 @@
 source "$PWD/utils.sh"
 
 cecho "RED" "Installing Virtualization Requirements ..."
-sudo pacman --noconfirm --needed -S virt-manager virt-viewer libvirt qemu vde2 ebtables dnsmasq bridge-utils openbsd-netcat
+sudo pacman --noconfirm --needed --yes -S virt-manager virt-viewer libvirt qemu vde2 ebtables dnsmasq bridge-utils openbsd-netcat
