c2d3c553194a2e646061b4963c807485df20a0f4
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-12-05 04:56:33 +0300

    minor change

--- partials/nvm.sh
+++ partials/nvm.sh
@@ -4,7 +4,8 @@ source "$PWD/utils.sh"
 
 cecho "RED" "Installing NVM ..."
 curl https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | sh
-source "$HOME/.zprofile"
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 cecho "RED" "Installing Node.js ..."
 nvm install --lts
 cecho "RED" "Updating NPM ..."
